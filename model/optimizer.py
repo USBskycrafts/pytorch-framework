2c32811488efad20c997c15d721989c6b8dcaca8
Junran Young <Junran Young>
2024-09-10 14:52:28 +0800

    feat(cleanup): refactor optimizer init for cleaner code

--- model/optimizer.py
+++ model/optimizer.py
@@ -1,7 +1,6 @@
 import torch.optim as optim
 from torch.optim.adam import Adam
 from torch.optim.sgd import SGD
-from pytorch_pretrained_bert import BertAdam
 
 
 def init_optimizer(model, config, *args, **params):
@@ -9,13 +8,10 @@ def init_optimizer(model, config, *args, **params):
     learning_rate = config.getfloat("train", "learning_rate")
     if optimizer_type == "adam":
         optimizer = Adam(model.parameters(), lr=learning_rate,
-                               weight_decay=config.getfloat("train", "weight_decay"))
+                         weight_decay=config.getfloat("train", "weight_decay"))
     elif optimizer_type == "sgd":
         optimizer = SGD(model.parameters(), lr=learning_rate,
-                              weight_decay=config.getfloat("train", "weight_decay"))
-    elif optimizer_type == "bert_adam":
-        optimizer = BertAdam(model.parameters(), lr=learning_rate,
-                             weight_decay=config.getfloat("train", "weight_decay"))
+                        weight_decay=config.getfloat("train", "weight_decay"))
     else:
         raise NotImplementedError
 
