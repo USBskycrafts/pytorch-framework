5e7b81bc316216668c4c91fdd708297cf6d237b5
Junran Young <Junran Young>
2024-10-14 17:08:25 +0800

    feat(symbiosis-v4d): 更新残差网络结构，引入可变形卷积提升性能

--- model/residual/res_net.py
+++ model/residual/res_net.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from .res_block import ResidualBlock
+from mmcv.ops import DeformConv2dPack
 
 
 class GeneratorResNet(nn.Module):
@@ -15,7 +16,7 @@ class GeneratorResNet(nn.Module):
         model = [  # model = [Pad + Conv + Norm + ReLU]
             # ReflectionPad2d(3):利用输入边界的反射来填充输入张量
             nn.ReflectionPad2d(3),
-            nn.Conv2d(channels, out_features, 7),  # Conv2d(3, 64, 7)
+            DeformConv2dPack(channels, out_features, 7),  # Conv2d(3, 64, 7)
             # InstanceNorm2d(64):在图像像素上对HW做归一化，用在风格化迁移
             nn.InstanceNorm2d(out_features),
             nn.LeakyReLU(inplace=True),  # 非线性激活
@@ -26,7 +27,8 @@ class GeneratorResNet(nn.Module):
         for _ in range(2):
             out_features *= 2  # out_features = 128 -> 256
             model += [  # (Conv + Norm + ReLU) * 2
-                nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+                DeformConv2dPack(in_features, out_features,
+                                 3, stride=2, padding=1),
                 nn.InstanceNorm2d(out_features),
                 nn.LeakyReLU(inplace=True),
             ]
@@ -42,7 +44,8 @@ class GeneratorResNet(nn.Module):
             out_features //= 2  # out_features = 128 -> 64
             model += [  # model += [Upsample + conv + norm + relu]
                 nn.Upsample(scale_factor=2),
-                nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+                DeformConv2dPack(in_features, out_features,
+                                 3, stride=1, padding=1),
                 nn.InstanceNorm2d(out_features),
                 nn.LeakyReLU(inplace=True),
             ]
@@ -51,7 +54,7 @@ class GeneratorResNet(nn.Module):
         # 网络输出层                                                            ## model += [pad + conv + tanh]
         # 将(3)的数据每一个都映射到[-1, 1]之间
         model += [nn.ReflectionPad2d(3),
-                  nn.Conv2d(out_features, output_dim, 7),
+                  DeformConv2dPack(out_features, output_dim, 7),
                   nn.Softplus()]
 
         self.model = nn.Sequential(*model)
--- train.py
+++ train.py
@@ -15,10 +15,12 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', '-c', help="specific config file", required=True)
+    parser.add_argument(
+        '--config', '-c', help="specific config file", required=True)
     parser.add_argument('--gpu', '-g', help="gpu id list")
     parser.add_argument('--checkpoint', help="checkpoint file path")
-    parser.add_argument('--do_test', help="do test while training or not", action="store_true")
+    parser.add_argument(
+        '--do_test', help="do test while training or not", action="store_true")
     parser.add_argument('--local_rank', help='local rank', default=0)
     args = parser.parse_args()
 
