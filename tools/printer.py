beb0ee5533bf8fb7bbcb4271e904b84c04c7a3b9
Junran Young <junran.young@outlook.com>
2024-10-22 13:42:42 +0800

    feat(printer.py): 修改打印功能以支持多进程，优化打印图像展示

--- tools/printer.py
+++ tools/printer.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import os
-from concurrent.futures import ProcessPoolExecutor
+import torch
 from multiprocessing import Process
-from threading import Lock
+from typing import Dict
+import numpy as np
+
 
 
 class Printer:
     def __init__(self, config):
         self.config = config
         self.num_worker = config.getint("output", "num_worker")
-        self.processor = ProcessPoolExecutor(max_workers=self.num_worker)
         self.path = config.get("output", "sample_path")
         self.model_name = config.get("output", "model_name")
         if not os.path.exists(f"{self.path}/{self.model_name}"):
@@ -23,28 +24,26 @@ class Printer:
             modal: image.detach().cpu() for modal, image in data.items()
         }
         job = Process(
-            target=self.print, args=(data, step), daemon=True)
+            target=self.print, args=(data, step))
         job.start()
 
-    def print(self, data, step):
-        fig, axes = plt.subplots(1, 4)
-        for i, (modal, image) in enumerate({
-            't1': data['t1'],
-            't2': data['t2'],
-            't1ce': data['t1ce'],
-            'pred': data['pred']
-        }.items()):
-            if len(image.shape) == 4:
-                bs, *_ = image.shape
-                image = image[bs // 2]
-            image = image.permute(1, 2, 0).detach().cpu().numpy()
-            ax = axes[i]
-            ax.set_title(
-                modal + f"#{data['number'][bs // 2]}, {data['layer'][bs // 2]}")
-            ax.axis('off')
-            ax.imshow(image, cmap='gray', vmin=0, vmax=1)
-        fig.tight_layout()
-        plt.savefig(f"{self.path}/{self.model_name}/{data['number'][bs // 2]}-{data['layer'][bs // 2]}.png",
-                    bbox_inches='tight', dpi=500)
-        plt.close(fig)
-        plt.clf()
+    def print(self, data: Dict[str, torch.Tensor], step):
+        modals = data.keys()
+        arrays = zip(*[torch.split(x, 1, dim=0) for x in data.values()])
+        
+        for images in arrays:
+            fig, axes = plt.subplots(1, 4)
+            view = [(modal, image) for modal, image in zip(modals, images) if modal in ['t1', 't2', 't1ce', 'pred']]
+            mark = {key: label.item() for key, label in zip(modals, images) if key in ['number', 'layer']}
+            for i, (modal, image) in enumerate(view):
+                image = image.squeeze(dim=0).permute(1, 2, 0).numpy()
+                ax = axes[i]
+                ax.set_title(
+                    modal + f"#{mark['number']}, {mark['layer']}")
+                ax.axis('off')
+                ax.imshow(image, cmap='gray', vmin=0, vmax=1)
+            fig.tight_layout()
+            plt.savefig(f"{self.path}/{self.model_name}/{mark['number']}-{mark['layer']}.png",
+                        bbox_inches='tight', dpi=500)
+            plt.close(fig)
+            plt.clf()
