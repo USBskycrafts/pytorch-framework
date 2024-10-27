bdacc717befe205f9dffb925efb9c85e2341c471
Junran.Y <junran.young@outlook.com>
2024-10-27 22:20:52 +0800

    feat(simbiosis-v9): 修改模型结构和参数配置

--- model/residual/res_net.py
+++ model/residual/res_net.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from .res_block import ResidualBlock
+from mmcv.ops import DeformConv2dPack
 
 
 class GeneratorResNet(nn.Module):
@@ -26,8 +27,8 @@ class GeneratorResNet(nn.Module):
         for _ in range(2):
             out_features *= 2  # out_features = 128 -> 256
             model += [  # (Conv + Norm + ReLU) * 2
-                nn.Conv2d(in_features, out_features,
-                          3, stride=2, padding=1),
+                DeformConv2dPack(in_features, out_features,
+                                 3, stride=2, padding=1),
                 nn.InstanceNorm2d(out_features),
                 nn.LeakyReLU(inplace=True),
             ]
@@ -43,8 +44,8 @@ class GeneratorResNet(nn.Module):
             out_features //= 2  # out_features = 128 -> 64
             model += [  # model += [Upsample + conv + norm + relu]
                 nn.Upsample(scale_factor=2),
-                nn.Conv2d(in_features, out_features,
-                          3, stride=1, padding=1),
+                DeformConv2dPack(in_features, out_features,
+                                 3, stride=1, padding=1),
                 nn.InstanceNorm2d(out_features),
                 nn.LeakyReLU(inplace=True),
             ]
--- model/symbiosis/net.py
+++ model/symbiosis/net.py
@@ -2,24 +2,13 @@ import torch
 import torch.nn as nn
 
 from tools.accuracy_tool import general_accuracy, general_image_metrics
-from model.user.crossformer import CrossformerEncoder, CrossformerDecoder
+from model.residual.res_net import GeneratorResNet
 
 
 class Symbiosis(nn.Module):
     def __init__(self, config, gpu_list, *args, **kwargs):
         super().__init__()
-        self.similarity_encoder = CrossformerEncoder(1, down_layers=3)
-        self.difference_encoder = CrossformerEncoder(1, down_layers=3)
-        self.decoder = CrossformerDecoder(1, up_layers=3)
-        self.cossine_loss = nn.CosineEmbeddingLoss(margin=0.5)
-        self.fuse_conv = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(512),
-            nn.SiLU(inplace=True),
-            nn.Conv2d(512, 256, kernel_size=1),
-            nn.InstanceNorm2d(256),
-            nn.SiLU(inplace=True),
-        )
+        self.model = GeneratorResNet(2, 1, 12)
         self.l1_loss = nn.L1Loss()
         print(self)
 
@@ -32,47 +21,8 @@ class Symbiosis(nn.Module):
             "t1ce": data["t1ce"],
             "t2": data["t2"],
         }
-        batch_size = config.getint('train', 'batch_size')
-        t1_similarity = self.similarity_encoder(data["t1"])
-        t1_difference = self.difference_encoder(data["t1"])
-        fused_feature = self.fuse_conv(
-            torch.cat([t1_similarity, t1_difference], dim=1))
-        pred = self.decoder(fused_feature)
-        loss = self.l1_loss(pred, data["t1ce"])
-        if mode != 'test':
-            t1_similarity = t1_similarity.reshape(batch_size, -1)
-            t1_difference = t1_difference.reshape(batch_size, -1)
-            t2_similarity = self.similarity_encoder(
-                data["t2"]).reshape(batch_size, -1)
-            t2_difference = self.difference_encoder(
-                data["t2"]).reshape(batch_size, -1)
-            t1ce_similarity = self.similarity_encoder(
-                data["t1ce"]).reshape(batch_size, -1)
-            t1ce_difference = self.difference_encoder(
-                data["t1ce"]).reshape(batch_size, -1)
-            loss += self.cossine_loss(t1_similarity,
-                                      t1ce_similarity,
-                                      torch.ones(batch_size, device=t1_similarity.device)) \
-                + self.cossine_loss(t1_similarity,
-                                    t2_similarity,
-                                    -torch.ones(batch_size, device=t1_similarity.device)) \
-                + self.cossine_loss(t1ce_similarity,
-                                    t2_similarity,
-                                    -torch.ones(batch_size,
-                                                device=t1_similarity.device)) \
-                + self.cossine_loss(t1_difference,
-                                    t2_difference,
-                                    torch.ones(batch_size,
-                                               device=t1_similarity.device)) \
-                + self.cossine_loss(t1_difference,
-                                    t1ce_difference,
-                                    -torch.ones(batch_size,
-                                                device=t1_similarity.device)) \
-                + self.cossine_loss(t1ce_difference,
-                                    t2_difference,
-                                    torch.ones(batch_size,
-                                               device=t1_similarity.device))
-
+        pred = self.model(torch.cat([data["t1"], data["t2"]], dim=1))
+        loss = self.l1_loss(pred, data['t1ce'])
         acc_result = general_image_metrics(
             pred, data["t1ce"], config, acc_result)
 
