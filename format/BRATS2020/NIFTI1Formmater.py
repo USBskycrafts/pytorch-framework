a31fcc8a811ec69d7a37437bd7d2b077402981c1
Junran.Y <junran.young@outlook.com>
2024-10-28 18:45:40 +0800

    feat: 调整训练参数和数据格式化方法

--- format/BRATS2020/NIFTI1Formmater.py
+++ format/BRATS2020/NIFTI1Formmater.py
@@ -20,15 +20,15 @@ class NIFTI1Formatter(BasicFormatter):
         number_list = [number for number in map(lambda x: x['number'], data)]
         layer_list = [number for number in map(lambda x: x['layer'], data)]
 
-        def generate_mask(label: torch.Tensor) -> torch.Tensor:
-            enhanceing_tumor = (label == 4) * torch.ones_like(label)
-            tumor_core = (label == 1) * torch.ones_like(label) + \
-                (label == 4) * torch.ones_like(label)
-            whole_tumor = (label == 1) * torch.ones_like(label) + (label == 2) * \
-                torch.ones_like(label) + (label == 4) * torch.ones_like(label)
-            mask = torch.cat(
-                [enhanceing_tumor, tumor_core, whole_tumor], dim=1)
-            return mask
+        # def generate_mask(label: torch.Tensor) -> torch.Tensor:
+        #     enhanceing_tumor = (label == 4) * torch.ones_like(label)
+        #     tumor_core = (label == 1) * torch.ones_like(label) + \
+        #         (label == 4) * torch.ones_like(label)
+        #     whole_tumor = (label == 1) * torch.ones_like(label) + (label == 2) * \
+        #         torch.ones_like(label) + (label == 4) * torch.ones_like(label)
+        #     mask = torch.cat(
+        #         [enhanceing_tumor, tumor_core, whole_tumor], dim=1)
+        #     return mask
         batch = {
             't1': torch.stack(t1_list, dim=0),
             't2': torch.stack(t2_list, dim=0),
@@ -45,21 +45,22 @@ class NIFTI1Formatter(BasicFormatter):
             aug = next(aug)
             aug['data'] = torch.from_numpy(aug['data'])
             t1, t2, t1ce, mask = torch.split(aug['data'], 1, dim=1)
-            aug = get_other_data_augmentation(
-                torch.cat([batch['t1'], batch['t2'],
-                           batch['t1ce']], dim=1).numpy())
-            aug = next(aug)
-            aug['data'] = torch.from_numpy(aug['data'])
-            t1, t2, t1ce = torch.split(aug['data'], 1, dim=1)
+            # aug = get_other_data_augmentation(
+            #     torch.cat([batch['t1'], batch['t2'],
+            #                batch['t1ce']], dim=1).numpy())
+            # aug = next(aug)
+            # aug['data'] = torch.from_numpy(aug['data'])
+            # t1, t2, t1ce = torch.split(aug['data'], 1, dim=1)
             assert batch['t1'].shape == t1.shape
             batch['t1'] = t1
             batch['t2'] = t2
             batch['t1ce'] = t1ce
-            batch['mask'] = (mask := generate_mask(mask))
-            assert torch.le(mask, 1).all() and torch.ge(mask, 0).all()
-        else:
-            batch['mask'] = generate_mask(batch['mask'])
-            assert torch.le(batch['mask'], 1).all(
-            ) and torch.ge(batch['mask'], 0).all()
+            batch['mask'] = mask
+            # batch['mask'] = (mask := generate_mask(mask))
+            # assert torch.le(mask, 1).all() and torch.ge(mask, 0).all()
+        # else:
+        #     # batch['mask'] = generate_mask(batch['mask'])
+        #     assert torch.le(batch['mask'], 1).all(
+        #     ) and torch.ge(batch['mask'], 0).all()
 
         return batch
