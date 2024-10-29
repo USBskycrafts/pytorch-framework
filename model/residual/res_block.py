b7a8bd99b9c53cf8ca47df888ab0f1ae97c1862d
Junran.Y <junran.young@outlook.com>
2024-10-29 11:49:44 +0800

    feat(symbiosis-v9r9): 更新网络及训练配置

--- model/residual/res_block.py
+++ model/residual/res_block.py
@@ -11,7 +11,7 @@ class ResidualBlock(nn.Module):
             nn.Conv2d(in_features, in_features, 3),  # 卷积
             # InstanceNorm2d():在图像像素上对HW做归一化，用在风格化迁移
             nn.InstanceNorm2d(in_features),
-            nn.SiLU(inplace=True),  # 非线性激活
+            nn.LeakyReLU(inplace=True),  # 非线性激活
             nn.ReflectionPad2d(1),  # ReflectionPad2d():利用输入边界的反射来填充输入张量
             DeformConv2dPack(in_features, in_features, 3),  # 卷积
             # InstanceNorm2d():在图像像素上对HW做归一化，用在风格化迁移
